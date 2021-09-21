6398c851609686b4b4d163dcabcc5bccf0074d0f
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-09-21 17:32:00 -0300

    updt

--- regex/phone_number.py
+++ regex/phone_number.py
@@ -1,5 +1,20 @@
 import re
-
-phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# Criando grupos com () em regex
+phone_num_regex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phone_num_regex.search('My number is 415-555-4242')
-print(f'Phone number found: ' + mo.group())
+print(f"Somente Codigo de area: {mo.group(1)}")
+print(f"Somente Numero telefone: {mo.group(2)}")
+print(f'Phone number complet found: ' + mo.group())
+
+# Obtendo todos os grupos de uma vez
+print(f"Todos os grupos de uma vez: {mo.groups()}")
+
+area_code, phone_number = mo.groups()
+print(f"Codigo de area {area_code}")
+print(f"Phone number: {phone_number}")
+
+# Escapando caracter () em regex
+phone_number_regex2 = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
+mo2 = phone_number_regex2.search('My number is  (415)-555-4242')
+print(f"Codigo de area com parenteses: {mo2.group(1)}")
+print(f"Apenas numero de telefone: {mo2.group(2)}")
